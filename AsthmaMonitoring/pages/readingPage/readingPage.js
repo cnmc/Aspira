ac9d3ab886e33c2ae0537e5fb50e91c1b9f748b5
Vasu Gupta <meetvasu15@gmail.com>
2013-03-18 01:19:02 -0700

    adding text boxes

--- AsthmaMonitoring/pages/readingPage/readingPage.js
+++ AsthmaMonitoring/pages/readingPage/readingPage.js
@@ -8,16 +8,55 @@
         // populates the page elements with the app's data.
         ready: function (element, options) {
             // TODO: Initialize the page here.
+           // WinJS.Utilities.id("PEFValue").listen("click", this.currReadingMonitor, false);
+         //   WinJS.Utilities.id("PEFValue").o
+            document.getElementById('middleContent').onkeypress = this.currReadingMonitor.bind(this);
+            var confirmBtnMarkup = document.createElement("button");
+            confirmBtnMarkup.id = "confirmButton";
+            confirmBtnMarkup.innerHTML = "Confirm";
+            confirmBtnMarkup.onclick = this.changeScreenFev;
+            document.getElementById('middleContent').appendChild(confirmBtnMarkup);
         },
+
+        currReadingMonitor: function (eventInfo) {
+            // validate input 
+           // if (validateInput("PEFValue")) {
+                
+           // }
+
+           
+        }, 
 
         unload: function () {
             // TODO: Respond to navigations away from this page.
+        }, 
+        changeScreenFev: function(){
+            document.getElementById("headingText").innerHTML = "FEV Reading";
+            document.getElementById("confirmButton").removeNode(true);
+            var confirmBtnFEVMarkup = document.createElement("button");
+            confirmBtnFEVMarkup.id = "confirmButton";
+            confirmBtnFEVMarkup.innerHTML = "Confirm";
+            confirmBtnFEVMarkup.onclick = function () {WinJS.Navigation.navigate("/pages/home/home.html");};
+            document.getElementById('middleContent').appendChild(confirmBtnFEVMarkup);
         },
-
+        
         updateLayout: function (element, viewState, lastViewState) {
             /// <param name="element" domElement="true" />
 
             // TODO: Respond to changes in viewState.
         }
     });
+
+    function validateInput(feildName) {
+        var currValueEntered = document.getElementById(feildName).value;
+        var re = new RegExp("\d+(\.\d{1,2})?");
+        var result = re.exec(currValueEntered);
+        if (result == null)
+            return false;
+        else
+            return true;
+    }
+
+   
+    
 })();
