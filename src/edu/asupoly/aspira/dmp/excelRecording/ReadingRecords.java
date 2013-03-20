07ed398e531f0f8fa6f30c1932f4d4367d2e04bd
rjbradsh <aheetbaybees@gmail.com>
2013-03-20 12:16:22 -0700

    Added an intended destination to save method

--- src/edu/asupoly/aspira/dmp/excelRecording/ReadingRecords.java
+++ src/edu/asupoly/aspira/dmp/excelRecording/ReadingRecords.java
@@ -1,5 +1,7 @@
 package edu.asupoly.aspira.dmp.excelRecording;
 
+import java.io.File;
+
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -9,7 +11,7 @@ import edu.asupoly.aspira.model.ParticleReading;
 
 public class ReadingRecords {
 
-	public static void ParticleandSpirometer(ParticleReading part , SpirometerReading spiro){
+	public static void ParticleandSpirometer(ParticleReading part , SpirometerReading spiro, File saveLocation){
 		Workbook wb = new HSSFWorkbook();
 		Sheet mySheet = wb.createSheet();
 	}
