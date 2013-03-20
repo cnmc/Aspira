57e48bdf4854ed8bbc353447e03a8b4ef2985b9b
rjbradsh <aheetbaybees@gmail.com>
2013-03-20 12:14:39 -0700

    Test case for excel spreadsheet creation

--- src/edu/asupoly/aspira/dmp/excelRecording/ReadingRecords.java
+++ src/edu/asupoly/aspira/dmp/excelRecording/ReadingRecords.java
@@ -1,4 +1,4 @@
-package edu.asupoly.aspira.dmp.excelsheets;
+package edu.asupoly.aspira.dmp.excelRecording;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Sheet;
---
+++ src/edu/asupoly/aspira/dmp/excelRecording/test/ReadingRecordsTest.java
@@ -0,0 +1,141 @@
+package edu.asupoly.aspira.dmp.excelRecording.test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.apache.poi.hssf.util.CellReference;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.asupoly.aspira.dmp.devicelogs.DeviceLogException;
+import edu.asupoly.aspira.dmp.excelRecording.ReadingRecords;
+import edu.asupoly.aspira.model.ParticleReading;
+import edu.asupoly.aspira.model.SpirometerReading;
+
+public class ReadingRecordsTest {
+
+	Workbook testCase;
+	Sheet testCaseSheet;
+	Workbook test;
+	Sheet testSheet;
+	
+	@Before
+	public void setUp() throws Exception {
+		testCase = WorkbookFactory.create(new File("ExcelTest.xls"));
+		testCaseSheet = testCase.getSheetAt(0);
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testParticleandSpirometer() throws DeviceLogException {
+		ArrayList<String> testStringCells = new ArrayList<String>();
+		ArrayList<Double> testDoubleCells = new ArrayList<Double>();
+		ArrayList<Date> testDateCells = new ArrayList<Date>();
+		ArrayList<Boolean> testBooleanCells = new ArrayList<Boolean>();
+		
+		ArrayList<String> testedStringCells = new ArrayList<String>();
+		ArrayList<Double> testedDoubleCells = new ArrayList<Double>();
+		ArrayList<Date> testedDateCells = new ArrayList<Date>();
+		ArrayList<Boolean> testedBooleanCells = new ArrayList<Boolean>();
+		
+		ParticleReading testParticles = new ParticleReading("01/01/01", "12:01", "3.0", "1.5");
+		SpirometerReading testSpiro = new SpirometerReading("pid", "01/01/01", "1", "2", "3.5", "6", "2");
+		
+		ReadingRecords.ParticleandSpirometer(testParticles, testSpiro, new File("WriteTest.xls"));
+		
+		try {
+			test = WorkbookFactory.create(new File("WriteTest.xls"));
+		} catch (InvalidFormatException e) {
+			assert(false);
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		testSheet = test.getSheetAt(0);
+		
+		
+		for (Row row : testCaseSheet) {
+	        for (Cell cell : row) {
+	            CellReference cellRef = new CellReference(row.getRowNum(), cell.getColumnIndex());
+	            System.out.print(cellRef.formatAsString());
+	            System.out.print(" - ");
+
+	            switch (cell.getCellType()) {
+	                case Cell.CELL_TYPE_STRING:
+	                    testStringCells.add(cell.getRichStringCellValue().getString());
+	                    break;
+	                case Cell.CELL_TYPE_NUMERIC:
+	                    if (DateUtil.isCellDateFormatted(cell)) {
+	                        testDateCells.add(cell.getDateCellValue());
+	                    } else {
+	                        testDoubleCells.add(cell.getNumericCellValue());
+	                    }
+	                    break;
+	                case Cell.CELL_TYPE_BOOLEAN:
+	                    testBooleanCells.add(cell.getBooleanCellValue());
+	                    break;
+	                case Cell.CELL_TYPE_FORMULA:
+	                    System.out.println(cell.getCellFormula());
+	                    break;
+	                default:
+	                    System.out.println();
+	            }
+	        }
+		}
+	        
+	    for (Row row : testSheet) {
+		        for (Cell cell : row) {
+		            CellReference cellRef = new CellReference(row.getRowNum(), cell.getColumnIndex());
+		            System.out.print(cellRef.formatAsString());
+		            System.out.print(" - ");
+
+		            switch (cell.getCellType()) {
+		                case Cell.CELL_TYPE_STRING:
+		                    testedStringCells.add(cell.getRichStringCellValue().getString());
+		                    break;
+		                case Cell.CELL_TYPE_NUMERIC:
+		                    if (DateUtil.isCellDateFormatted(cell)) {
+		                        testedDateCells.add(cell.getDateCellValue());
+		                    } else {
+		                        testedDoubleCells.add(cell.getNumericCellValue());
+		                    }
+		                    break;
+		                case Cell.CELL_TYPE_BOOLEAN:
+		                    testedBooleanCells.add(cell.getBooleanCellValue());
+		                    break;
+		                case Cell.CELL_TYPE_FORMULA:
+		                    System.out.println(cell.getCellFormula());
+		                    break;
+		                default:
+		                    System.out.println();
+		            }
+		        }
+	    }
+	    
+	    assertEquals(testStringCells, testedStringCells);
+	    assertEquals(testDateCells, testedDateCells);
+	    assertEquals(testDoubleCells, testedDoubleCells);
+	    assertEquals(testBooleanCells, testedBooleanCells);
+	    
+	}
+		
+}
+
