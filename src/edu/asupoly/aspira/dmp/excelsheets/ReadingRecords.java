9c5645730fe2adcc4908216f6742611265562371
rjbradsh <aheetbaybees@gmail.com>
2013-03-20 11:03:14 -0700

    ReadingRecords object commit

---
+++ src/edu/asupoly/aspira/dmp/excelsheets/ReadingRecords.java
@@ -0,0 +1,16 @@
+package edu.asupoly.aspira.dmp.excelsheets;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import edu.asupoly.aspira.model.SpirometerReading;
+import edu.asupoly.aspira.model.ParticleReading;
+
+public class ReadingRecords {
+
+	public static void ParticleandSpirometer(ParticleReading part , SpirometerReading spiro){
+		Workbook wb = new HSSFWorkbook();
+		Sheet mySheet = wb.createSheet();
+	}
+}
