f9d6146184eac01d385b84241224792e6f0b5d91
Kevin Gary <kgary@asu.edu>
2013-04-01 12:01:42 -0700

    Updated Medication unit test to conform to new Constructor

--- src/edu/asupoly/aspira/test/MedicationScheduleTest.java
+++ src/edu/asupoly/aspira/test/MedicationScheduleTest.java
@@ -10,9 +10,9 @@ public class MedicationScheduleTest {
 
 	@Test
 	public void test() {
-		MedicationSchedule dinamizine1= new MedicationSchedule("dinamizine", "0");
-		MedicationSchedule dinamizine2 = new MedicationSchedule("dinamizine", "1");
-		MedicationSchedule madeupipran = new MedicationSchedule("madeupipran","2");
+		MedicationSchedule dinamizine1= new MedicationSchedule("dinamizine", "0", "1");
+		MedicationSchedule dinamizine2 = new MedicationSchedule("dinamizine", "1", "1");
+		MedicationSchedule madeupipran = new MedicationSchedule("madeupipran","2", "1");
 		
 		assertTrue(dinamizine1.equals(dinamizine2));
 		assertFalse(dinamizine1.equals(madeupipran));
